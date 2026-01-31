4aa457c1faceb164cff9fbb8305e40bbd720441a
Jovan Doder <jovandoder0@gmail.com>
2026-01-31 04:08:56 +0100

    vite updated

--- armagedonleskovac/vite.config.ts
+++ armagedonleskovac/vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
+base: "/armagedonleskovac/",
+
   server: {
     host: "::",
     port: 8080,
--- krismobil/vite.config.ts
+++ krismobil/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+base: "/krismobil/",
   server: {
     host: "::",
     port: 8080,
